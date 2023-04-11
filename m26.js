3e384ed39c523fcf10044739c7f37cc0c4a25499
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2023-04-11 17:55:54 -0300

    Add files via upload

---
+++ m26.js
@@ -0,0 +1,9 @@
+"use strict";
+function calculo(num1, num2) {
+    return num1 * num2;
+}
+function nome(nome) {
+    return `Ola ${nome}`;
+}
+console.log(nome('Denis'));
+console.log(calculo(10, 2));
---
+++ m26.ts
@@ -0,0 +1,13 @@
+function calculo(num1: number, num2: number): number {
+    return num1 * num2
+} 
+
+
+function nome(nome: string): string {
+    return `Ola ${nome}`;
+}
+
+
+console.log(nome('Denis'))
+
+console.log(calculo(10, 2))
